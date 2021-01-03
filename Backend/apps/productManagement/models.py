8e12747e8960f56cd451eb52c3abaf9d9f5da748
Damurq <michaelmontero.idb@gmail.com>
2021-01-03 00:19:33 -0400

    v-1 modelos app productManagement

--- Backend/apps/productManagement/models.py
+++ Backend/apps/productManagement/models.py
@@ -1,19 +1,31 @@
 from django.db import models
+from django.db.models.fields.related import ForeignObject
+
+MARCAS = (
+    ("X","Xiaomi"),
+    ("A","Apple"),
+    ("S","Samsung"),
+    ("M","Motorola"),
+)
+ESTADO_FISICO = (
+    ("N","Nuevo"),
+    ("U","Usado"),
+)
 
 class Producto(models.Model):
     ESTADOS = (
-        ("A","Activo"
-         "I","Inactivo"
-         "SS","Sin Stock"),
+        ("A","Activo"),
+        ("I","Inactivo"),
+        ("SS","Sin Stock"),
     )
     TIPOS = (
         ("T","Telefono"),
-        ("A","Accesorio(s)"),
+        ("A","Articulo(s)"),
         ("S","Servicio"),
-        ("TA","Telefono y accesorio"),
-        ("TAS","Telefono, accesorio(s) y servicio"),
+        ("TA","Telefono y Articulo"),
+        ("TAS","Telefono, Articulo(s) y servicio"),
         ("TS","Telefono y servicio"),
-        ("AS","Accesorio y servicio"),
+        ("AS","Articulo y servicio"),
         ("V","Vacio"),
     )
     nombre = models.CharField(max_length=255)
@@ -31,7 +43,7 @@ class Producto(models.Model):
         return self.nombre
 
     class Meta:
-        """Propiedades adicionales del modelo Empleado"""
+        """Propiedades adicionales del modelo"""
         db_table = "Producto"
 
 class Promocion(models.Model):
@@ -59,18 +71,139 @@ class Promocion(models.Model):
         db_table="Promocion"
 
 class Fecha(models.Model):
+    ESTADOS = (
+        ("A","Activo"),
+        ("I","Inactivo"),
+        ("F","Finalizado"),
+        ("E","En espera")
+    )
     promocion_Id = models.Foreignkey('Promocion', on_delete=models.CASCADE)
     fechaInicio = models.DateField()
     fechaExpiracion = models.DateField()
-    estado = models.DateField()
+    estado = models.CharField(max_length=2)
 
-class Accesorio(models.Model):
-    nombre = models.CharField()
-    descripcion = models.Charfield()
-    estado = models.CharField(max_length=1)
+    def __srt__(self):
+        return self.fechaInicio
 
-class Telefono(models.Model):
-    nombre = models.CharField()
-    modelo = models.charfield()
-    marca = models.CharField()
+    class Meta:
+        db_table = "Fecha"
+
+class Articulo(models.Model):
+    ESTADOS = (
+        ("A","Activo"),
+        ("I","Inactivo"),
+        ("SS","Sin stock"),
+    )
+    TIPO = (
+        ("A","Accesorio"),
+        ("T","Telefono"),
+    )
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    marca = models.CharField(max_length=1, choices=MARCAS)
+    producto_Articulo = models.ManyToManyField('Producto')
+    foto = models.ImageField(
+        upload_to = 'images/articule/%y/%m/%d',
+        default ='images/articule/default.jpg',
+        max_length = 255
+    )
+    estado_fisico = models.CharField(max_field=1,choices=ESTADO_FISICO)
+    tipo = models.CharField(max_field=1,choices=TIPO)
+    estado = models.CharField(max_length=2, CHOICES=ESTADOS, default="A")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = "Articulo"
+
+class Servicio(models.Model):
+    ESTADOS = (
+        ("A","Activo"),
+        ("I","Inactivo"),
+    )
+    producto_Id = models.OneToOneField('Producto',on_delete=models.CASCADE)
+    nombre = models.Charfield(max_length=255)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=20,descimal_place=3)
+    foto = models.ImageField(
+        upload_to='images/services/%y/%m/%d',
+        default='image/services/default.jpg',
+        max_length=255
+    )
+    estado = models.CharField(max_length=1,choices=ESTADOS,default="A")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        """Propiedades adicionales del modelo"""
+        db_table = "Servicio"
+
+class Modelo(models.Model):
+    ESTADOS = (
+        ("A","Activo"),
+        ("I","Inactivo"),
+    )
+    sku = models.CharField(primary_key=True, unique=True) #falta generar codigo
+    Articulo_Id = models.ForeignKey('Articulo',on_delete=models.CASCADE)
+    nombre = models.Charfield(max_length=255)
+    precio = models.DecimalField(max_digits=20,descimal_place=3)
+    stock = models.PositiveSmallIntegerField()
+    foto = models.ImageField(
+        upload_to = 'images/model/%y/%m/%d',
+        default ='images/model/default.jpg',
+        max_length = 255
+    )
+    estado = models.CharField(max_length=2, CHOICES=ESTADOS, default="A")
     
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        """Propiedades adicionales del modelo"""
+        db_table = "Modelo"
+
+class Caracteristica (models.Model):
+    ESTADOS = (
+        ("A","Activo"),
+        ("I","Inactivo"),
+    )
+    TIPO = (
+        ("AP","Afecta el precio"),
+        ("NP","No afecta el precio"),
+    )
+    #Si afecta el precio solo puede tener un valor
+    #Si no afecta el precio puede tener varios valores
+    modelo_Id = models.ForeignKey('Modelo',on_delete=models.CASCADE)
+    variante_Id = models.ForeignKey('Variante',on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=2, CHOICES=TIPO)
+    estado = models.CharField(max_length=1, CHOICES=ESTADOS, default="A")
+        
+    def __str__(self):
+        return self.modelo_Id
+
+    class Meta:
+        """Propiedades adicionales del modelo"""
+        db_table = "Caracteristica"
+
+class Valor (models.Model):
+    caracteristica_id = models.ForeignKey('Caracteristica',on_delete=models.CASCADE)
+    valor = models.CharField(max_length=255)
+            
+    def __str__(self):
+        return self.caracteristica_id
+
+    class Meta:
+        """Propiedades adicionales del modelo"""
+        db_table = "Valor"
+
+class Variante(models.Model):
+    nombre = models.CharField(max_length=255)
+                
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        """Propiedades adicionales del modelo"""
+        db_table = "Variante"
